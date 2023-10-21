64644a58263d3a4cc2baff8e7a1f0f005af7a8a4
faruktinaz <faruktinazz@icloud.com>
2023-10-21 20:26:59 +0300

    added ex00 files

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+	}
+	std::cout << "\n";
+	return (0);
+}
