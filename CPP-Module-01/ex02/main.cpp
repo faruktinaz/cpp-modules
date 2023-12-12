6bc64ac85f5eb82ef51aaa51d8e8a03f7c2f14a8
faruktinaz <faruktinazz@icloud.com>
2023-12-12 23:30:12 +0300

    update ex02

--- CPP-Module-01/ex02/main.cpp
+++ CPP-Module-01/ex02/main.cpp
@@ -1,16 +1,15 @@
  #include <iostream>
 
-int main()
-{
-	std::string str = "*HI THIS IS BRAIN*";
-	std::string *stringPTR = &str;
-	std::string &stringREF = str;
+int main(){
+    std::string Astring = "HI THIS IS BRAIN";
+    std::string *stringPTR = &Astring;
+    std::string &stringREF = Astring;
 
-	std::cout << "str degişkeninin bellek adresi: " << &str << std::endl;
-	std::cout << "stringPTR değişkeninin bellek adresi: " << stringPTR << std::endl;
-	std::cout << "stringREF değişkeninin bellek adresi: " << &stringREF << std::endl;
-	std::cout << "\n";
-	std::cout << "str değişkeninin değeri: " << str << std::endl;
-	std::cout << "stringPTR değişkeninin değeri: " << *stringPTR << std::endl;
-	std::cout << "stringREF değişkeninin değeri: " << stringREF << std::endl;
+    std::cout << &Astring << std::endl;
+    std::cout << &stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    std::cout << Astring << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
 }
